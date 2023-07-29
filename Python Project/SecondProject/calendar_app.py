8eb9bf4487e085d586b73deea6d4c6558c644e60
ayonsom <140237905+ayonsom@users.noreply.github.com>
2023-07-29 20:02:02 +0530

    Update calendar_app.py
    Display calendar in GUI using tkinter library in python. Second project given by MasaiSchool.

--- Python Project/SecondProject/calendar_app.py
+++ Python Project/SecondProject/calendar_app.py
@@ -1,1 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+from tkcalendar import Calendar
 
+def get_date():
+    selected_date = cal.selection_get()
+    label.config(text=f"Selected Date: {selected_date}")
+
+root = tk.Tk()
+root.title("Calendar App")
+
+cal = Calendar(root, selectmode="day", year=2023, month=7, day=29)
+cal.pack(pady=20)
+
+button = ttk.Button(root, text="Get Date", command=get_date)
+button.pack(pady=10)
+
+label = ttk.Label(root, text="Selected Date: ")
+label.pack(pady=10)
+
+root.mainloop()
